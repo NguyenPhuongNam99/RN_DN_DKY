4f240e58e4aec0eda7ab163950c64020abc909b6
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-01-20 18:14:25 +0700

    test day

--- Component/Profile.js
+++ Component/Profile.js
@@ -17,7 +17,7 @@ const Profile =()=>{
         source={{uri:'https://scontent.fhan3-1.fna.fbcdn.net/v/t1.0-9/83040435_175874177153572_7388585565899194368_n.jpg?_nc_cat=102&ccb=2&_nc_sid=174925&_nc_ohc=CzyMQ9oPIVEAX9aEnEE&_nc_ht=scontent.fhan3-1.fna&oh=674b15ff9b646250e21d1b40829798db&oe=602C2A05'}}/>
            </View>
             <View style={styles.header}>
-                <Text style={{fontWeight:'bold',fontSize:18}}>Nguyễn Phương Nam</Text>
+                <Text style={{fontWeight:'bold',fontSize:18}}>Nguyễn Phương Naam</Text>
                 <Text style={{color:'#3784FF',fontSizeL:12,fontWeight:'bold'}}>Thay đổi ảnh đại diện</Text>
             </View>
         </View>
